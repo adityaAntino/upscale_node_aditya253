d6ed1368df67cd4017809166246b090dc3402568
Aditya Arya <aarya@coligomed.com>
2023-07-26 15:23:52 +0530

    crud operations completed

--- index.js
+++ index.js
@@ -1,44 +1,24 @@
 
 const express = require('express');
-const req = require('express/lib/request');
 const app = express();
 require('dotenv').config();
 const env  = require('./env/config');
 const port = process.env.PORT || 5500;
 const crudRouter = require('./src/app/crud')
 
-app.use("./crud",crudRouter);
+app.use(express.json({}));
 
+app.get('/',async(req,res) =>{
+console.log('server running 1');
+return res.status(200).json({
+    message: "application is working."
+})
+});
 
-app.listen(env.port,()=>{
+app.use("/crud",crudRouter);
+
+app.listen(port, ()=>{
     console.log(`Server Started at Port ${port}`);
 })
 
  
-///CREATION & ROUTING OF NODE SERVER USING EXPRESS
-
-// const server = http.createServer((req, res) => {
-//     const { method, url } = req;
-//         console.log(`method ${method} url ${url}`)
-//     if (method === 'GET' && url === '/') {
-    
-//         serverHeader(res, 200, 'text/plain');
-//         res.end('Welcome to Home Page!');
-//     }
-//     else if (method === 'GET' && url === '/about') {
-//         serverHeader(res, 200, 'text/plain');
-//         res.end('This is about page');
-//     }
-//     else if (method === 'POST' && url === '/submit') {
-//         serverHeader(res, 200, 'text/html');
-//         res.end('Request Submitted');
-//     }
-//     else {
-//         serverHeader(400);
-//         res.end('Page not found', 'text/plain');
-//     }
-// });
-
-// server.listen(port, hostname, () => {
-//     console.log(`Server is running at http://${hostname}:${port}`)
-// });
--- src/app/crud.js
+++ src/app/crud.js
@@ -2,17 +2,132 @@ const router = require('express').Router();
 
 var primaryData = [];
 
-router.get('/get-info', function (req, res) {
+
+
+router.get('/get-info', async (req, res) => {
     try {
+        console.log(`Get info called ${primaryData}`);
+
         return res.status(200).json({
             "message": "Data Fetched Successfully",
             "statusCode": 200,
-            primaryData: primaryData
+            data: primaryData
         });
-    } catch {
+    } catch (error) {
         console.log(`Get info Error ${error}`);
         return res.status(500).json({
             "message": "Failed to fetch info",
+            "statusCode": 500
+        });
+    }
+});
+
+router.post('/add-data', async (req, res) => {
+    const { data } = req.body;
+    try {
+        if (data != null) {
+            console.log(`ADD DATA CALLED ${data}`);
+            primaryData.push(data);
+            return res.status(200).json({
+                message: "Data Added Successfully",
+                data: primaryData
+            });
+        }
+        else {
+            return res.status(400).json({ "message": "Data is null" });
+        }
+    } catch (error) {
+        console.log(`ADD DATA Error ${error}`);
+        return res.status(500).json({
+            "message": "Failed to add data",
+            "statusCode": 500
+        });
+    }
+})
+
+router.delete('/delete-all', async (req, res) => {
+    try {
+        console.log('Delete All Data Called');
+        primaryData.pop();
+        return res.status('200').json({
+            message: "Data Deleted Successfully",
+            data: primaryData
+        });
+    }
+    catch (error) {
+        console.log(`Delete Data Error ${error}`);
+        return res.status(500).json({
+            "message": "Failed to delete all data",
+            "statusCode": 500
+        });
+    }
+});
+
+router.delete('/delete-one', async (req, res) => {
+    const { value } = req.body;
+    try {
+        console.log(`Delete one called ${value}`);
+        if (primaryData.includes(value) || value != null) {
+            const dataIndex = primaryData.findIndex((item) => (value === item));
+            if (dataIndex === -1) {
+                return res.status(400).json({
+                    message: "Index of value not available in array",
+                    statusCode: 400
+                });
+            } else {
+                primaryData.splice(dataIndex, 1);
+                return res.status(200).json({
+                    "message": "Singular Data Deleted Successfully",
+                    data: primaryData
+                });
+            }
+        } else {
+            return res.status(400).json({
+                message: "Value not available in array",
+                statusCode: 400
+            });
+        }
+
+    } catch (error) {
+        console.log(`Delete Single Data Error ${error}`);
+        return res.status(500).json({
+            "message": "Failed to delete all data",
+            "statusCode": 500
+        });
+    }
+});
+
+router.put('/update-data', async (req, res) => {
+    const { previous, next } = req.body;
+    try {
+        console.log(`Update Data Called previous = ${previous} next = ${next}`);
+
+        if (primaryData.includes(previous)) {
+            const dataIndex = primaryData.findIndex((item) => (item === previous));
+
+            if (dataIndex === -1) {
+                res.status(400).json({
+                    "message": "Index not available to update requested data",
+                    "statusCode": 400,
+                });
+            }
+            primaryData[dataIndex] = next;
+            return res.status(200).json({
+                "message": "Data updated successfully",
+                data: primaryData
+            });
+            
+        } else {
+            res.status(400).json({
+                "message": "Data Not Available to update",
+                "statusCode": 400,
+            });
+        }
+    }
+    catch (error) {
+        console.log(`Update Data Error ${error}`);
+        return res.status(500).json({
+            "message": "Failed to update data",
             "statusCode": 500
         });
     }

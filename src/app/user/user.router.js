4547d48f77b78e673702f92b686e005afa6f1c58
Aditya Arya <aarya@coligomed.com>
2023-07-31 15:52:33 +0530

    get-all-user added

--- src/app/user/user.router.js
+++ src/app/user/user.router.js
@@ -7,9 +7,11 @@ const { decodeToken } = require('./user.middleware')
 
 router.get('/get-user-details', decodeToken, userController.getUserDetails);
 
+router.get('/get-all-user', decodeToken, userController.getAllUser);
+
 router.put('/update-user-details', decodeToken, userController.updateUserDetails);
 
-router.delete('/delete-user',decodeToken, userController.deleteUser);
+router.delete('/delete-user', decodeToken, userController.deleteUser);
 
 module.exports = router;
 

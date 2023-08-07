bd692f0eac778fe596198f830fa5183ddff05378
Aditya Arya <aarya@coligomed.com>
2023-08-07 20:06:33 +0530

    File Upload Added Successfully

---
+++ src/app/file_upload/file.upload.controller.js
@@ -0,0 +1,15 @@
+const router = require('express').Router()
+
+
+exports.uploadFile = async (req, res) => {
+    try {
+        if (!req.file) {
+            return res.status(400).json({ error: 'No file uploaded.' });
+        }
+        res.status(200).json({ message: 'File uploaded successfully.', filename: req.file.filename });
+    }
+    catch (error) {
+        return res.status(500).json({ error: 'Failed to upload file.' });
+    }
+
+}
